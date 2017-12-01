30457f7177a1a0c048589452ce1e515acab391ab
C Mogged <cmogged@gmail.com>
2017-12-01 14:02:52 -0600

    Update platformer.c

--- 3DPlatformer/platformer.c
+++ 3DPlatformer/platformer.c
@@ -73,7 +73,7 @@ void loadImg(int imgObjNum, char *imgFile)
    int x, y;
    struct stat stat_p;
    FILE *bmpFile;
-   int imgWidth;
+   int imgWidth, roughImgWidth, powOf2Limiter;
    int headerSize;
    
    // does the file exist
@@ -88,7 +88,10 @@ void loadImg(int imgObjNum, char *imgFile)
    
    //get image width
    //the image must be square and have a width with a power of 2
-   imgWidth = pow(2, log(sqrt(stat_p.st_size/3))/log(2));
+   //the header must be small enough relative to the image data
+   roughImgWidth = sqrt(stat_p.st_size / 3);//3 bytes per pixel
+   powOf2Limiter = log(roughImgWidth) / log(2) + 0.5;//add 0.5 to round
+   imgWidth = pow(2, powOf2Limiter);
    
    //read image file to buffer
    headerSize = stat_p.st_size - imgWidth * imgWidth * 3;
